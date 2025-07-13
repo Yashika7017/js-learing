180daae8b902dbf224d82c2d6500ba2ed2f7deb8
Yashika7017 <114479419+Yashika7017@users.noreply.github.com>
2025-07-13 07:12:49 +0000

    dataTypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,27 @@
+"use strict"; // treated all js as a newer js vresion
+
+let name = "Yashika"
+let age = 20
+let isLoggedIn = true
+
+// basically all rights side values are data types
+
+// string => ""
+// number
+// bigint
+// boolean => true/false
+// null => standalone value , when I print types of null then I see this is a object type
+// undefine 
+// symbole => unique
+
+
+// object 
+
+console.log(typeof "yashika");
+console.log(typeof null);
+console.log(typeof Object);
+console.log(typeof undefined);
+
+
+
+
