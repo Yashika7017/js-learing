e4daa5ff5ad0f60dd2f2377c155cfca483dff551
Yashika7017 <114479419+Yashika7017@users.noreply.github.com>
2025-07-13 13:10:24 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = undefined
+console.log(typeof score);
+console.log(score);
+
+
+let numValue = Number(score)
+console.log(typeof numValue);
+console.log(score);
+
+
+let boolValue = Boolean(score)
+console.log(typeof boolValue);
+console.log(score);
+
+
+let objValue = Object(score)
+console.log(typeof objValue);
+console.log(score);
+
