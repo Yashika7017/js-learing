14f985df21c2593547b59838eea7aa35ebcaaf7b
Yashika7017 <114479419+Yashika7017@users.noreply.github.com>
2025-07-14 11:15:27 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,12 @@
+// Comparision and Equality check they both are different .
+// (===) this operator check first datatype and then compare value but (==) this is only compare the values without check datatype
+
+console.log(null < 2);
+console.log(null == 2);
+console.log(null <= 2);
+
+console.log(undefined < 2);
+console.log(undefined == 2);
+console.log(undefined <= 2);
+
+// these type of comparisions are avoid because they are create the confusion and time weasting
