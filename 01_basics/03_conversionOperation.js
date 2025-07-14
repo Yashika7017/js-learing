598f0e3eb35c0b4d25e4f42c5cebd944ec39e692
Yashika7017 <114479419+Yashika7017@users.noreply.github.com>
2025-07-14 04:20:18 +0000

    conversion, prefix & postfix

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,3 +17,47 @@ let objValue = Object(score)
 console.log(typeof objValue);
 console.log(score);
 
+
+
+// --><--><--><--><--><--><--><--><--><--> OPERATIONS <--><--><--><--><--><--><--><--><--><--><--><
+
+
+let value = 3
+let negvalue = -value
+console.log(value);
+console.log(negvalue);
+console.log(typeof negvalue);
+
+let strValue = String (value)
+console.log(typeof strValue);
+console.log(strValue);
+
+let str1 = "Hello" ;
+let str2 = "  Shiva" ; 
+console.log (str1 + str2);
+console.log (str1 - str2);
+console.log (str1 * str2);
+console.log (str1 ** str2);
+console.log (str1 / str2);
+console.log (str1 % str2);
+
+
+let cal1 = 2 ;
+let cal2 = 2 ;
+console.log(cal1 + cal2);
+console.log(cal1 - cal2);
+console.log(cal1 * cal2);
+console.log(cal1 ** cal2);
+console.log(cal1 / cal2);
+console.log(cal1 % cal2);
+
+
+let gameCounter = 100 ;
+console.log(gameCounter++);
+console.log(++gameCounter);
+console.log(gameCounter--);
+console.log(--gameCounter);
+console.log(--gameCounter);
+console.log(gameCounter);
+
+
