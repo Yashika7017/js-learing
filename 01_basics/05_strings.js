b98f007937650fbb3f3c1480b2e7b42724b5ba7f
Yashika Varshney <114479419+Yashika7017@users.noreply.github.com>
2025-07-15 13:57:25 +0000

    strings methods

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,16 @@
+const str = "Yashika";
+let myAge = 20 ;
+// string interpolation method
+console.log(`Hello My name is ${str} and now I'm ${myAge} Years old`);
+
+const companyName = new String ("Google");
+console.log(companyName);
+
+console.log(companyName.charAt(4));
+console.log(companyName.indexOf("g"));
+console.log(companyName.__proto__);
+console.log(companyName[1,2]);
+console.log(companyName[0,1,2]);
+console.log(companyName.toUpperCase());
+
+
