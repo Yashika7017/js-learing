7099507a9e395bd3c0877f805379c76975fca36d
Yashika Varshney <114479419+Yashika7017@users.noreply.github.com>
2025-07-16 13:07:53 +0000

    learn numbers and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,27 @@
+const score = 400
+console.log(score);
+
+console.log(typeof score);
+
+let balance = new Number (100)
+console.log(balance);
+console.log(balance.toString());
+console.log(typeof balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(3));
+
+
+const otherNumber = 1234.999
+console.log(otherNumber.toPrecision(3));
+
+const rupee = 10000000
+console.log(rupee.toLocaleString("en-IN"));
+
+// ++++++++++++++++++++++++++++++++++++   MATHS IN JAVASCRIPT    +++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-452));
+console.log(Math.round(1391.11));
+console.log(Math.ceil(3.1));
+console.log(Math.floor(3.1));
+console.log(Math.sqrt(25));
