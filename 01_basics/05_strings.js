e49df2a90f6c5b51c67abd917ddb87e16f00555c
Yashika Varshney <114479419+Yashika7017@users.noreply.github.com>
2025-07-16 07:19:02 +0000

    apply common strings method

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -14,3 +14,21 @@ console.log(companyName[0,1,2]);
 console.log(companyName.toUpperCase());
 
 
+const name = "Yashika Varshney";
+let age = 20;
+let course = "B.Tech [(C.S.E) final year student]"
+const myStr = (`Hello my name is ${name}. I'm ${age} years old.I'm currently in ${course} `)
+console.log(myStr);
+console.log(myStr.length);
+console.log(myStr.slice(0,3));
+console.log(myStr.toUpperCase());
+console.log(myStr.toLocaleLowerCase());
+console.log(myStr.toLocaleUpperCase());
+console.log(myStr.toString());
+
+console.log(myStr.indexOf("Yashika"));
+console.log(myStr.lastIndexOf("Yashika"));
+
+console.log(myStr.includes("Hello" ));
+console.log(myStr.trim(myStr));
+
