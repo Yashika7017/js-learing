683556eb8038aeab575d286d49394ae300601fc2
Yashika Varshney <114479419+Yashika7017@users.noreply.github.com>
2025-07-17 12:46:58 +0000

    Array

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,28 +1,28 @@
 //////////////////////////////////////  ARRAY    /////////////////////////////////////
 
-// const myArr = [0,1,2,3,4,5]
-// console.log(myArr);
-// console.log(typeof myArr); // object
+const myArr = [0,1,2,3,4,5]
+console.log(myArr);
+console.log(typeof myArr); // object
 
   
 let myArr2 = new Array(5,4,3,2,1,0)
-// console.log(myArr2);
-// console.log(typeof myArr2); // object
+console.log(myArr2);
+console.log(typeof myArr2); // object
 
 /////////////////////////////////////   ARRAY-METHODS   ///////////////////////////////
 
-// myArr2.push(2);
-// myArr2.pop()
-// console.log(myArr2);
+myArr2.push(2);
+myArr2.pop()
+console.log(myArr2);
  
-// myArr2.unshift(2);
-// myArr2.shift();
-// console.log(myArr2);
+myArr2.unshift(2);
+myArr2.shift();
+console.log(myArr2);
 
-// console.log(myArr2.includes(2));
-// console.log(myArr2.includes(9));
-// console.log(myArr2.indexOf(1));
-// console.log(myArr2.indexOf(9));
+console.log(myArr2.includes(2));
+console.log(myArr2.includes(9));
+console.log(myArr2.indexOf(1));
+console.log(myArr2.indexOf(9));
 
 
 
