e4c9df0cf1dfec494fadcd382ae03de4ebb5326d
Yashika Varshney <114479419+Yashika7017@users.noreply.github.com>
2025-07-17 06:31:50 +0000

    date and times

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -1,15 +1,15 @@
 ////////////////////////////////     DATES    //////////////////////////////////////// 
 
-// let myDate = new Date()
-// console.log(myDate);
-// console.log(myDate.toString());
-// console.log(myDate.toDateString());
-// console.log(myDate.toISOString());
-// console.log(myDate.toLocaleDateString());
-// console.log(myDate.toTimeString());
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toTimeString());
 
  let createdDate = new Date(17,06,2025)
-// console.log(createdDate.toLocaleString());
+console.log(createdDate.toLocaleString());
 
 let timeStamp = Date.now()
 console.log(timeStamp);
