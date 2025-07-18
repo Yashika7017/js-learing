4152f387c3d1a03f09091a76ea534f051e04d313
Yashika Varshney <114479419+Yashika7017@users.noreply.github.com>
2025-07-18 07:07:21 +0000

    learn part 2

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,40 +1,51 @@
-//////////////////////////////////////  ARRAY    /////////////////////////////////////
+// //////////////////////////////////////  ARRAY    /////////////////////////////////////
 
-const myArr = [0,1,2,3,4,5]
-console.log(myArr);
-console.log(typeof myArr); // object
+// const myArr = [0,1,2,3,4,5]
+// console.log(myArr);
+// console.log(typeof myArr); // object
 
   
-let myArr2 = new Array(5,4,3,2,1,0)
-console.log(myArr2);
-console.log(typeof myArr2); // object
+// let myArr2 = new Array(5,4,3,2,1,0)
+// console.log(myArr2);
+// console.log(typeof myArr2); // object
 
-/////////////////////////////////////   ARRAY-METHODS   ///////////////////////////////
+// /////////////////////////////////////   ARRAY-METHODS   ///////////////////////////////
 
-myArr2.push(2);
-myArr2.pop()
-console.log(myArr2);
+// myArr2.push(2);
+// myArr2.pop()
+// console.log(myArr2);
  
-myArr2.unshift(2);
-myArr2.shift();
-console.log(myArr2);
+// myArr2.unshift(2);
+// myArr2.shift();
+// console.log(myArr2);
 
-console.log(myArr2.includes(2));
-console.log(myArr2.includes(9));
-console.log(myArr2.indexOf(1));
-console.log(myArr2.indexOf(9));
+// console.log(myArr2.includes(2));
+// console.log(myArr2.includes(9));
+// console.log(myArr2.indexOf(1));
+// console.log(myArr2.indexOf(9));
 
-///////////////////////////////////////   SLICE and SPLICE  /////////////////////////////////////
+// ///////////////////////////////////////   SLICE and SPLICE  /////////////////////////////////////
 
-const sli1 = ["yellow", "orange", "red", "purple", "baby-pink","white"];
-let sli2 = sli1.slice(1,4);
-console.log(sli1);
-console.log(sli2); // slice orignal array se koi chhed chhad nhi krta
+// const sli1 = ["yellow", "orange", "red", "purple", "baby-pink","white"];
+// let sli2 = sli1.slice(1,4);
+// console.log(sli1);
+// console.log(sli2); // slice orignal array se koi chhed chhad nhi krta
 
-const spli1 = ["yellow", "orange", "red", "purple", "baby-pink","white"];
-let spli2 = spli1.splice(1,4);
-console.log(spli1);
-console.log(spli2); // splice orignal array ko hi change/manipulate kr deta hai
+// const spli1 = ["yellow", "orange", "red", "purple", "baby-pink","white"];
+// let spli2 = spli1.splice(1,4);
+// console.log(spli1);
+// console.log(spli2); // splice orignal array ko hi change/manipulate kr deta hai
+
+
+
+//  ARRAY PART-2 :-->
+
+let score1 = 1000;
+let score2 = 1000;
+let score3 = 1000;
+let score4 = 1000;
+
+console.log(Array.of(score1, score2, score3, score4));
 
 
 
