5eb05a9ff72cd818f173a5b70daff3587ee1c005
Yashika Varshney <114479419+Yashika7017@users.noreply.github.com>
2025-07-22 13:51:03 +0000

    this keyword

---
+++ 02_basics/05_arrow.js
@@ -0,0 +1,10 @@
+const user = {
+    username : "Yashika Varshney",
+    price : 9999,
+
+    welcomeMessage : function () {
+        console.log(`${this.username}, Welcome to website`);
+        
+    }
+}
+user.welcomeMessage()
