52d6d8c745d6ccbea49636614ccec4b34d6b9ca8
Yashika Varshney <114479419+Yashika7017@users.noreply.github.com>
2025-07-22 13:03:39 +0000

    learn global and local scope

--- 02_basics/04_scope.js
+++ 02_basics/04_scope.js
@@ -1,18 +1,51 @@
 
-let c = 900
+var c = 900
 if (true){
     
     let a = 10
     const b = 20
-    let c = 30
+    var c = 30
+}
+
+console.log(a); // error
+console.log(b); // erroe
+console.log(c); // 30
+
+function one (){
+    const username = "Yashika Varshney"
+
+    function two (){
+        const website = "YouTube"
+        console.log(username);
+        console.log(website);
+    }
+    
+    two ()
+    
+}
+one()
+
+if(true){
+    const username = "Radha-rani"
+    if(username === "Radha-rani"){
+        const website = "  Campus-key"
+        console.log(username + website);
+        console.log(website);
+    }
+    
+    console.log(username);
+}
+
+
+console.log(addone(8));
+ 
+function addone (num) {
+    return num + 1 ; 
 }
 
 
 
-
-
-
-
-//console.log(a);
-//console.log(b);
-console.log(c);
+const addtwo = function (num){
+    return num + 2 ;
+} 
+console.log(addtwo(1));
