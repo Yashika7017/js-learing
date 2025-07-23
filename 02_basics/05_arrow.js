51d4f2462240f20fa88629f55b82614d601292db
Yashika Varshney <114479419+Yashika7017@users.noreply.github.com>
2025-07-23 10:05:21 +0000

    learn arrow function

--- 02_basics/05_arrow.js
+++ 02_basics/05_arrow.js
@@ -1,10 +1,46 @@
-const user = {
-    username : "Yashika Varshney",
-    price : 9999,
+// const user = {
+//     username : "Yashika Varshney",
+//     price : 9999,
 
-    welcomeMessage : function () {
-        console.log(`${this.username}, Welcome to website`);
+//     welcomeMessage : function () {
+//         console.log(`${this.username}, Welcome to website`);
+//         console.log(this); // provide the current constant
         
-    }
+//     }
+// }
+// user.welcomeMessage()
+// user.username = "Shiva"
+// user.welcomeMessage()
+
+
+// function check (){
+//     let username = "Yashika"
+//     console.log(this.username);
+    
+// }
+// check()
+
+// const check = function(){
+//     //let username = "Yashika"
+//     console.log(this);
+    
+// }
+// check()
+
+//////////////////////////////   ARROW FUNCTION   ////////////////////////////////////////////
+const chai = () =>  {
+    let username = "Yashika"
+    console.log(this.username);
+    
 }
-user.welcomeMessage()
+chai()
+
+// const addTwo =(num1, num2) => {
+//     return num1 + num2;
+// }
+// console.log(addTwo(2,6));
+
+///////////////////////////////   IMPLICIT RETURN   /////////////////////////////
+
+const mulTwo =(num1, num2) =>  num1 * num2;
+console.log(mulTwo(2,6));
