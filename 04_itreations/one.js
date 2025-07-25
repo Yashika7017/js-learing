e6cfee05da051c9c53c6a3a27ad5da29d588cf9e
Yashika Varshney <114479419+Yashika7017@users.noreply.github.com>
2025-07-25 13:12:13 +0000

    learn for loop

--- 04_itreations/one.js
+++ 04_itreations/one.js
@@ -1,4 +1,4 @@
-//###########################@ :-->  FOR  <--: @###################################
+//###########################@ :-->  FOR LOOP  <--: @###################################
 
 for (let i = 1; i <= 10; i++) {
     const element = i*9;
@@ -6,3 +6,51 @@ for (let i = 1; i <= 10; i++) {
 }
 
 
+
+for (let i = 1; i <= 10; i ++){
+   console.log(" TABLE OF :- " + i);
+    
+    for (let j = 1; j <= 10; j++){
+       // console.log("Inner loop" + j);
+        console.log(`${i} * ${j}  =  ${i*j}`);
+        
+        
+    }
+}
+
+
+
+const MNCs = ["Google", "Microsoft", "Apple", "Infosys", "Samsung", "Oracle", "Amazon"];
+console.log(MNCs.length);
+
+for (let i = 0; i < MNCs.length; i++) {
+    const element = MNCs[i];
+    console.log(`The Top MNC's is ${i*1} :--> ${element}`);
+    
+}
+
+
+//////////////////// BREAK (or) CONTINUE :-->
+
+for (let index = 1; index <=10; index++) {
+    const element = index;
+    if (element == 5) {
+        console.log("Detected = 5");
+        break;
+    }
+    console.log(element);
+    
+    
+}
+
+
+ for (let index = 1; index <=10; index++) {
+    const element = index;
+    if (element == 5) {
+        console.log("Detected = 5");
+        continue;
+    }
+    console.log(element);
+    
+    
+}
