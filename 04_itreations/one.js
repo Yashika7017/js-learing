5b75d63f641ad4a8ea10a57c59490782989fa73e
Yashika Varshney <114479419+Yashika7017@users.noreply.github.com>
2025-07-26 08:20:37 +0000

    somthing new

--- 04_itreations/one.js
+++ 04_itreations/one.js
@@ -1,68 +1,66 @@
-// //###########################@ :-->  FOR LOOP  <--: @###################################
+//###########################@ :-->  FOR LOOP  <--: @###################################
 
-// for (let i = 1; i <= 10; i++) {
-//     const element = i*9;
-//     console.log(element);
-// }
-
-
-
-// for (let i = 1; i <= 10; i ++){
-//    console.log(" TABLE OF :- " + i);
-    
-//     for (let j = 1; j <= 10; j++){
-//        // console.log("Inner loop" + j);
-//         console.log(`${i} * ${j}  =  ${i*j}`);
-        
-        
-//     }
-// }
-
-
-
-// const MNCs = ["Google", "Microsoft", "Apple", "Infosys", "Samsung", "Oracle", "Amazon"];
-// console.log(MNCs.length);
-
-// for (let i = 0; i < MNCs.length; i++) {
-//     const element = MNCs[i];
-//     console.log(`The Top MNC's is ${i*1 + 1} :--> ${element}`);
-    
-// }
-
-
-// //////////////////// BREAK (or) CONTINUE :-->
-
-// for (let index = 1; index <=10; index++) {
-//     const element = index;
-//     if (element == 5) {
-//         console.log("Detected = 5");
-//         break;
-//     }
-//     console.log(element);
-    
-    
-// }
-
-
-//  for (let index = 1; index <=10; index++) {
-//     const element = index;
-//     if (element == 5) {
-//         console.log("Detected = 5");
-//         continue;
-//     }
-//     console.log(element);
-    
-    
-// }
-
-
-// function my_fun (n) {
-let my_fun = ("hii", "hello", "name", "a", "b", "c", "d","e","f","g");
-for (let i = 0; i <= 10; i++) {
-    const element = my_fun[i];
+for (let i = 1; i <= 10; i++) {
+    const element = i*9;
     console.log(element);
-        
-        
 }
+
+
+
+for (let i = 1; i <= 10; i ++){
+   console.log(" TABLE OF :- " + i);
     
+    for (let j = 1; j <= 10; j++){
+       // console.log("Inner loop" + j);
+        console.log(`${i} * ${j}  =  ${i*j}`);
+        
+        
+    }
+}
+
+
+
+const MNCs = ["Google", "Microsoft", "Apple", "Infosys", "Samsung", "Oracle", "Amazon"];
+console.log(MNCs.length);
+
+for (let i = 0; i < MNCs.length; i++) {
+    const element = MNCs[i];
+    console.log(`The Top MNC's is ${i*1 + 1} :--> ${element}`);
+    
+}
+
+
+//////////////////// BREAK (or) CONTINUE :-->
+
+for (let index = 1; index <=10; index++) {
+    const element = index;
+    if (element == 5) {
+        console.log("Detected = 5");
+        break;
+    }
+    console.log(element);
+    
+    
+}
+
+
+ for (let index = 1; index <=10; index++) {
+    const element = index;
+    if (element == 5) {
+        console.log("Detected = 5");
+        continue;
+    }
+    console.log(element);
+    
+    
+}
+
+
+function my_fun (n) {
+    for (let i = 0; i <= n; i++) {
+        // const element = array[i];
+        console.log(i);
+        
+    }
+}my_fun(8)
 
