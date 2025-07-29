e73186eefe2bd7ecb57b61fcbba49fac7bea5821
Yashika Varshney <114479419+Yashika7017@users.noreply.github.com>
2025-07-29 09:07:44 +0000

    filter

---
+++ 04_itreations/four.js
@@ -0,0 +1,12 @@
+// const coding = ["js", "java", "c++", "python", "SQL", "My SQL", "ruby", "c#"];
+
+// const value = coding.forEach ( (n) => {
+//      console.log(n);
+//      return n ;
+// });
+// console.log(value);
+
+
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const new_num = num.filter( (items) => items > 2);
+console.log(new_num);
