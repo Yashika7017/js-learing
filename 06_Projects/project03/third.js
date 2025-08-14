4793d02932c95cf63e344545d66551b9dd4cec4b
Yashika Varshney <114479419+Yashika7017@users.noreply.github.com>
2025-08-14 12:33:01 +0000

    Digital Clock

--- 06_Projects/project03/third.js
+++ 06_Projects/project03/third.js
@@ -2,7 +2,7 @@ const clock = document.getElementById('clock');
 
 setInterval( () => {
     let date = new Date();
-    // console.log(date.toLocaleDateString());
+    console.log(date.toLocaleDateString());
     // clock.innerHTML= date.toLocaleString();
     
 },1000 );
